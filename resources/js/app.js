9ff085b5a384b33eb9819b681dc4b99293bd4a6e
Fernando Lamas <lamas.fernando1419@gmail.com>
2021-08-16 15:47:02 -0300

    Added VueJs 2.6.x version manually

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,15 @@
 require('./bootstrap');
+import Components from 'laravel-mix/src/components/Components';
+// require('alpinejs');
 
-require('alpinejs');
+import Vue from 'vue';
+
+Vue.component('ExampleComponent', require('./components/ExampleComponent.vue').default); // one way for importing  and registering component
+// import ExampleComponent from "./components/ExampleComponent.vue"; // only imports the component
+
+const app = new Vue({
+    el: '#app',
+    //    components: {
+    //        ExampleComponent // registers it.
+    //    }
+});
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Hello, Example Component!
+    <h3> I'll be displayed in the guest.blade.php views. </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log("Example component mounted.");
+  },
+};
+</script>
